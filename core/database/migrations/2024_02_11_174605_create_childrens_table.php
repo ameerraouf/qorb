121c9e4469fb1df0c22e779274fd85590337ed43
wolf <ahmedragabyasin2020@gmail.com>
2024-02-11 20:57:02 +0200

    register page

--- core/database/migrations/2024_02_11_174605_create_childrens_table.php
+++ core/database/migrations/2024_02_11_174605_create_childrens_table.php
@@ -11,15 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('teachers', function (Blueprint $table) {
+        Schema::create('childrens', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->unsignedTinyInteger('status')->default(0);
-            $table->enum('type', ['teacher', 'mother'])->default('teacher');
-            $table->rememberToken();
+            $table->integer('age');
+            $table->text('problem');
             $table->timestamps();
         });
     }
@@ -29,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('teachers');
+        Schema::dropIfExists('childrens');
     }
 };
